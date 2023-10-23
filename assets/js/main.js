b72ae6323c4af718f42a34d2defb89e0f6a98171
unknown <20131a4408@gvpce.ac.in>
2023-10-23 15:10:38 +0530

    files uploaded

---
+++ assets/js/main.js
@@ -0,0 +1,67 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+const navMenu = document.getElementById('nav_menu'),
+     navToggle=document.getElementById('nav_toggle'),
+     navClose=document.getElementById('nav_close')
+
+/*===== MENU SHOW =====*/
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click',()=>{
+        // adds a new class called show_menu to navmenu Element where classlist return a list of classes present to particular element
+        navMenu.classList.add('show_menu')
+    })
+}
+
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',()=>{
+        navMenu.classList.remove('show_menu')
+    })
+}
+ 
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink=document.querySelectorAll('.nav_link')
+function link(){
+    const navMenu=document.getElementById('nav_menu')
+    navMenu.classList.remove('show_menu')
+}
+navLink.forEach(n=>n.addEventListener('click',link))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillscontent=document.getElementsByClassName('skills_content'),
+      skillsheader=document.querySelectorAll('.skills_header')
+function toggleskills(){
+    let item=this.parentNode.className
+    for(let i=0;i<skillscontent.length;i++){
+        skillscontent[i].className='skills_content skills_close'
+    }
+    if(item === 'skills_content skills_close'){
+        this.parentNode.className='skills_content skills_open'
+    }
+}
+skillsheader.forEach((el)=>{
+    el.addEventListener('click',toggleskills)
+})
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
